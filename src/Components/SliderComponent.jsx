5ad38717810bd19af0a0dae6238f7e741fb341a6
Srishty Bedi <70388131+srishty-07@users.noreply.github.com>
2022-08-23 17:28:26 +0530

    Update SliderComponent.jsx

--- src/Components/SliderComponent.jsx
+++ src/Components/SliderComponent.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import { EmissionState } from '../Context';
-import { road_material } from '../cost_geo';
 
-const PriceEmission = () => {
-  const { roadData, priceChange, ghgChange } = EmissionState();
+const SliderComponent = (props) => {
+    const {roadData, setRoadData} = EmissionState();
   return (
-    <i>
-      <i><div style={{ color: "#a98467", fontSize: "26px", fontFamily: 'Open Sans, sans-serif' }}><p>Total Cost: ₹<u><span style={{ color: ((roadData.map((it) => it.volume * (road_material.find((i) => i.name === it.name).cost)).reduce((acc, el) => acc += el, 0)) / 10000000).toFixed(2) > priceChange ? 'red' : ((roadData.map((it) => it.volume * (road_material.find((i) => i.name === it.name).cost)).reduce((acc, el) => acc += el, 0)) / 10000000).toFixed(2) < priceChange ? 'green' : 'black' }}>{`${((roadData.map((it) => it.volume * (road_material.find((i) => i.name === it.name).cost)).reduce((acc, el) => acc += el, 0)) / 10000000).toFixed(2)}`}</span></u> Cr</p></div></i>
-      <i><div style={{ color: "#a98467", fontFamily: 'Open Sans, sans-serif', fontSize: "26px" }}><p>Total CO2 emission: <u><span style={{ color: roadData.map((it) => it.volume * (road_material.find((i) => i.name === it.name).Carbon_emission)).reduce((acc, el) => acc += el, 0).toFixed(2) > ghgChange ? 'red' : roadData.map((it) => it.volume * (road_material.find((i) => i.name === it.name).Carbon_emission)).reduce((acc, el) => acc += el, 0).toFixed(2) < ghgChange ? 'green' : 'black' }}>{`${roadData.map((it) => it.volume * (road_material.find((i) => i.name === it.name).Carbon_emission)).reduce((acc, el) => acc += el, 0).toFixed(2)}`}</span></u> tCO2</p></div></i>
-    </i>
-
+    <div style={{ fontFamily: 'Open Sans, sans-serif' }}>
+        <p>{props.rd.name}</p>
+        <label>
+            <input type = "range" min = "0" max="50000" step="0.01" style={{color:"pink"}} value={props.rd.volume} onChange={(e)=>setRoadData(roadData.map((it)=>{if(it.name===props.rd.name){return {name:`${props.rd.name}`, volume:parseFloat(e.target.value)}}else{return it}}))}/>
+            {props.rd.volume}
+        </label>
+    </div>
   )
 }
 
-export default PriceEmission
+export default SliderComponent
